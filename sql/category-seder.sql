e224139d0bf3d1352243cdbfdc2409c213688c9a
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-09-29 16:59:48 -0500

    sql categories seeder file added

---
+++ sql/category-seder.sql
@@ -0,0 +1,9 @@
+USE `task-manager_db`;
+
+INSERT INTO categories (title) VALUES
+                                   ('work'),
+                                   ('chores'),
+                                   ('academic'),
+                                   ('finance'),
+                                   ('health'),
+                                   ('misc.');
