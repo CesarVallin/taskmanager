65ad8be3dd67f1023346240be205bf5ff0b63561
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-10-01 18:16:10 -0500

    able to update a task, schedule checkbox not checked

--- src/main/java/com/example/taskmanager/controllers/TaskController.java
+++ src/main/java/com/example/taskmanager/controllers/TaskController.java
@@ -6,6 +6,7 @@ import com.example.taskmanager.models.User;
 import com.example.taskmanager.repositories.CategoryRepository;
 import com.example.taskmanager.repositories.TaskRepository;
 import com.example.taskmanager.repositories.UserRepository;
+import org.springframework.security.access.method.P;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -73,6 +74,44 @@ public class TaskController {
                 return "redirect:/profile";
             }
         }
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/task/edit/{id}")
+    public String editIndividualTask(@PathVariable long id, Model model) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (taskDao.existsById(id)) {
+            Task taskToEdit = taskDao.findById(id).get();
+            if(taskToEdit.getUser().getId() == loggedInUser.getId()) {
+                List<Category> categories = categoryDao.findAll();
+                model.addAttribute("categories", categories);
+                model.addAttribute("task", taskToEdit);
+
+                return "task/task-edit";
+            }
+        }
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/task/edit/{id}")
+    public String updateEditedTask(@ModelAttribute Task task, @PathVariable long id){
+        /** User*/
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User currentUser = userDao.findById(loggedInUser.getId()).get();
+
+        /** Category - to set a category id for the task */
+        Long categoryId = task.getCategory().getId();
+        Category selectedCategory = categoryDao.findById(categoryId).get();
+
+        Task taskToUpdate = taskDao.findById(id).get();
+
+        // NEED TO ACTUALLY SAVE IT IN FULL including complete/incomplete
+        taskToUpdate.setTitle(task.getTitle());
+        taskToUpdate.setDescription(task.getDescription());
+        taskToUpdate.setDateScheduled(task.getDateScheduled());
+        taskToUpdate.setCategory(selectedCategory);
+        taskToUpdate.setUser(currentUser);
+        taskDao.save(taskToUpdate);
         return "redirect:/profile";
     }
 }
